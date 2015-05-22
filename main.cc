75cc699f489b1451f26537662555a221b4d11c6b
black13 <jjosburn@gmail.com>
2015-05-22 12:19:35 -0500

    added boost test framework

--- main.cc
+++ main.cc
@@ -5,14 +5,20 @@
 #include <string>
   using std::string;
 
+#define BOOST_TEST_MODULE First_TestSuite
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( my_test )
+{
+//    my_class test_object( "qwerty" );
+
+    //BOOST_CHECK( test_object.is_valid() );
+}
 #include <boost/program_options.hpp>
   namespace po = boost::program_options;
-
-
-
-int main(
-   void
-)
+/*
+int main(void)
 {
     return 0;
 }
+*/
