22067d28199d2cb2355e46b3126a1c8412c4b4da
black13 <jjosburn@gmail.com>
2015-05-22 11:29:16 -0500

    basic files

---
+++ main.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+  using std::cerr;
+  using std::cout;
+  using std::endl;
+#include <string>
+  using std::string;
+
+#include <boost/program_options.hpp>
+  namespace po = boost::program_options;
+
+
+
+int main(
+   void
+)
+{
+    return 0;
+}
